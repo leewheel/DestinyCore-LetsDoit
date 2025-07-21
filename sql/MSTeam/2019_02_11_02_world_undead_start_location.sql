3b30138fe57a320e66f82d6500b53e9f9fecfb7a
slash-design <elite_slash@hotmail.com>
2025-07-21 15:52:45 +0200

    Fix Typo

--- sql/MSTeam/2019_02_11_02_world_undead_start_location.sql
+++ sql/MSTeam/2019_02_11_02_world_undead_start_location.sql
@@ -271,11 +271,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (38910*100+1, 9, 6, 0, 0, 0, 100, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Lilian Voss - On Script - Set Event Phase 1");
 UPDATE creature_text SET emote = 18 WHERE CreatureID = 38910 AND GroupID = 0 AND ID = 0;
 UPDATE creature SET spawntimesecs = 60 WHERE id = 49428;
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` = 49428);
 DELETE FROM creature_template_addon WHERE entry = 49428;
 INSERT INTO creature_template_addon (entry, bytes2, emote) VALUES
 (49428, 1, 333);
 UPDATE creature SET spawndist = 3, MovementType = 1 WHERE id IN (49422, 49423);
-UPDATE creature_template SET AIName = "SmartAI" WHERE entry = 49424;
+UPDATE creature_template SET unit_flags = 33280, AIName = "SmartAI" WHERE entry = 49424;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (49422, 49423, 49424) AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (49422, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Rotbrain Berserker - On Aggro - Say Text Line 0 (No Repeat)"),
@@ -292,8 +293,8 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (49424, 0, 0, 1, 4, 0, 100, 1, 0, 0, 0, 0, 11, 83015, 0, 0, 0, 0, 0, 21, 25, 0, 0, 0, 0, 0, 0, "Marshal Redpath - On Aggro - Cast Spell 'Heroic Leap' (No Repeat)"),
 (49424, 0, 1, 0, 61, 0, 100, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Marshal Redpath - On Aggro (Link) - Say Text Line 0 (No Repeat)"),
 (49424, 0, 2, 0, 0, 0, 100, 1, 5000, 9000, 6000, 10000, 11, 33239, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Marshal Redpath - In Combat - Cast Spell 'Whirlwind'");
-DELETE FROM creature_text WHERE entry IN (49422, 49423, 49424);
-INSERT INTO creature_text (entry, groupid, id, text, type, language, probability, emote, duration, sound, comment, BroadcastTextID) VALUES
+DELETE FROM creature_text WHERE CreatureID IN (49422, 49423, 49424);
+INSERT INTO creature_text (CreatureID, GroupID, ID, Text, Type, Language, Probability, Emote, Duration, Sound, Comment, BroadcastTextID) VALUES
 (49422, 0, 0, "I'll kill you!", 12, 0, 15, 0, 0, 0, "Rotbrain Berserker", 50324),
 (49422, 0, 1, "Look what they've done to me!", 12, 0, 15, 0, 0, 0, "Rotbrain Berserker", 50325),
 (49422, 0, 2, "Who am I?", 12, 0, 15, 0, 0, 0, "Rotbrain Berserker", 50320),
