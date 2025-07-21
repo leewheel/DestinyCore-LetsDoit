9e3dad53f5c28f1cefbd1330725c1e695fb6ac15
slash-design <elite_slash@hotmail.com>
2025-07-21 10:41:20 +0200

    Core/DB: Add more dh spells
    185123 // 226778 // 206476 // 179057 // 212105 // 212106 // 207760 // 205411 // 236189

--- sql/MSTeam/01_02_2019_world.sql
+++ sql/MSTeam/01_02_2019_world.sql
@@ -29,26 +29,43 @@ DELETE FROM `spell_script_names` WHERE `spell_id`=199845 AND `ScriptName`='spell
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (199845, 'spell_pri_psyflay_area');
 
-+-- Frostbrand 
-+delete from `spell_script_names` where ScriptName = 'spell_sha_hailstorm';
-+insert into `spell_script_names` values ('196834', 'spell_sha_hailstorm');
-+
-+-- Mastery: Combo Strikes
-+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes';
-+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
-+(100780, 'spell_monk_mastery_combo_strikes'),
-+(185099, 'spell_monk_mastery_combo_strikes'),
-+(123586, 'spell_monk_mastery_combo_strikes'),
-+(100784, 'spell_monk_mastery_combo_strikes');
-+
-+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes_periodic_auras';
-+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
-+(113656, 'spell_monk_mastery_combo_strikes_periodic_auras'),
-+(101546, 'spell_monk_mastery_combo_strikes_periodic_auras'),
-+(152175, 'spell_monk_mastery_combo_strikes_periodic_auras');
-+
-+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes_periodic_triggers';
-+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
-+(117418, 'spell_monk_mastery_combo_strikes_periodic_triggers'),
-+(107270, 'spell_monk_mastery_combo_strikes_periodic_triggers'),
-+(158221, 'spell_monk_mastery_combo_strikes_periodic_triggers');
+-- Frostbrand 
+delete from `spell_script_names` where ScriptName = 'spell_sha_hailstorm';
+insert into `spell_script_names` values ('196834', 'spell_sha_hailstorm');
+
+-- Mastery: Combo Strikes
+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes';
+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
+(100780, 'spell_monk_mastery_combo_strikes'),
+(185099, 'spell_monk_mastery_combo_strikes'),
+(123586, 'spell_monk_mastery_combo_strikes'),
+(100784, 'spell_monk_mastery_combo_strikes');
+
+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes_periodic_auras';
+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
+(113656, 'spell_monk_mastery_combo_strikes_periodic_auras'),
+(101546, 'spell_monk_mastery_combo_strikes_periodic_auras'),
+(152175, 'spell_monk_mastery_combo_strikes_periodic_auras');
+
+delete from `spell_script_names` where `ScriptName`='spell_monk_mastery_combo_strikes_periodic_triggers';
+insert into `spell_script_names` (`spell_id`, `ScriptName`) values
+(117418, 'spell_monk_mastery_combo_strikes_periodic_triggers'),
+(107270, 'spell_monk_mastery_combo_strikes_periodic_triggers'),
+(158221, 'spell_monk_mastery_combo_strikes_periodic_triggers');
+
+-- Cauterize
+DELETE FROM `spell_script_names` WHERE ScriptName = 'spell_mage_cauterize';
+INSERT INTO spell_script_names VALUES (86949, 'spell_mage_cauterize');
+
+
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (185123, 226778, 206476, 179057, 212105, 212106, 207760, 205411, 236189);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+( 185123, 'spell_dh_throw_glaive'),
+( 226778, 'spell_dh_infernal_strike_timer'),
+( 206476, 'spell_dh_momentum'),
+( 179057, 'spell_dh_chaos_nova'),
+( 212105, 'spell_dh_fel_devastation_damage'),
+( 212106, 'spell_dh_fel_devastation_heal'),
+( 207760, 'spell_dh_burning_alive'),
+( 205411, 'spell_dh_desperate_instincts'),
+( 236189, 'spell_dh_demonic_infusion');
