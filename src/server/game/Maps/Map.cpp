f1acbb7d38d22f6e886f7c406ab8d7ca235849ec
slash-design <elite_slash@hotmail.com>
2025-08-04 12:45:50 +0200

    Core/Map: fix previous commit
    Ref.: https://github.com/welder1976/MagicStorm/commit/78c926bc8614ae6cbea65aa045d999ca8af10903

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -3779,7 +3779,7 @@ bool Map::IsDungeon() const
 
 bool Map::IsScenario() const
 {
-    return i_mapEntry/* && i_mapEntry->IsScenario()*/; // need fix! not found in MapEntry!
+    return i_mapEntry && i_mapEntry->IsScenario();
 }
 
 bool Map::IsNonRaidDungeon() const
